a5a3a51fc412f02bb527b6c303b7b5be98a34ac4
YeeSen <36952285+isshenye@users.noreply.github.com>
2022-03-29 23:55:59 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -103,7 +103,6 @@ module.exports = {
           items: [
             {
               html: `<a href="https://isshenye.github.io/StaticRepo/sponsor/" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" ></a>`,
-              <iframe src="https://isshenye.github.io/StaticRepo/sponsor/" style="overflow-x:hidden;overflow-y:hidden; border:0xp none #fff; min-height:240px; width:100%;" frameborder="0" scrolling="no"></iframe>
             },
           ],
         },
